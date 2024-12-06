7d6f42fd8d4ba157074773a4b67ac498d77c95ea
vladislavzotov <vladislavzotov@skyscanner.net>
2024-12-06 15:22:13 +0100

    add build and launch example script

---
+++ build-and-launch-example.sh
@@ -0,0 +1,19 @@
+# Vanilla
+cd client-sdk/vanilla
+npm install
+npm run build
+
+# React
+cd ../react
+npm install
+npm run build
+
+# Node
+cd ../../node-sdk
+npm install
+npm run build
+
+# Node React Example
+cd ../examples/node-react
+npm install
+npm run start
