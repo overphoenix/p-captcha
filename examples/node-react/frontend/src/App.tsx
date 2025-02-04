328bedf3daa576f9188391e7da50ffb0891f9644
vladislavzotov <vladislavzotov@skyscanner.net>
2025-02-04 19:32:59 +0100

    disable button for empty captchas

--- examples/node-react/frontend/src/App.tsx
+++ examples/node-react/frontend/src/App.tsx
@@ -190,7 +190,8 @@ function App() {
 
             <button
               type="submit"
-              className="w-full py-3 px-8 bg-blue-600 hover:bg-blue-700 text-white text-lg font-medium rounded-full transition duration-300"
+              disabled={!answer}
+              className="w-full py-3 px-8 bg-blue-600 hover:bg-blue-700 text-white text-lg font-medium rounded-full transition duration-300 disabled:opacity-50 disabled:cursor-not-allowed"
             >
               Submit
             </button>
